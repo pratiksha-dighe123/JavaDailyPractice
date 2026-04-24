8412ab14afee928189f79ea1821451596467373b
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-04-24 16:28:32 +0530

    Typecasting code

---
+++ src/TypeCastiing.java
@@ -0,0 +1,8 @@
+public class TypeCastiing {
+    public static void main(String[] args){
+        byte b=10;
+        int a = 10;
+//        byte b= a;                ....cast error
+        System.out.println(a);
+    }
+}
