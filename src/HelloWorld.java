85609a65fd099ea223a45ee72dead6a80e307a79
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-04-24 15:29:59 +0530

    intro to datatypes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        int num1 = 10;
-        int num2= 20;
-        int sum= num1 + num2;
-        System.out.println(sum);
+//        System.out.println("Hello, World!");
+//        int num1 = 10;
+//        int num2= 20;
+//        int sum= num1 + num2;
+//        System.out.println(sum);
+        char c='a';
+        c++;
+        System.out.println(c);
     }
 }
