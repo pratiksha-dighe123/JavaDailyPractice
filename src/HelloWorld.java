f75557f1ca59014c0f4aa0ec65e3ebe6f4c14869
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-04-24 15:01:45 +0530

    into to variable

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int num1 = 10;
+        int num2= 20;
+        int sum= num1 + num2;
+        System.out.println(sum);
     }
 }
