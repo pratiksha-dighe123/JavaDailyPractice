8f444c3cfb52dcde15a0f5355f0f2141fb954eec
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-04-26 22:08:52 +0530

    practicing code

---
+++ src/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+    public static void main(String args[]) {
+    int a=10;
+    int b=20;
+    System.out.println("The sum of a and b is: " + (a+b));
+    a++;
+    System.out.println("The value of a after increment is: " + a);
+    ++a;
+    System.out.println("The value of a after pre-increment is: " + a);
+
+    }
+}
