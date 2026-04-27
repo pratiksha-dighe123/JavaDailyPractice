bef951de90b03d2cbec7ed432a7b74e1ce252687
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-04-27 15:14:49 +0530

    ternary operator

--- src/Demo.java
+++ src/Demo.java
@@ -2,7 +2,8 @@ public class Demo {
     public static void main(String args[]) {
     int num=10;
 
-    int result = (num % 2 ==0? 10 :20);
-    System.out.println(result);
+    //ternary operator
+    String result = (num % 2 == 0) ? "Even" : "Odd";
+    System.out.println(num + " is " + result);
     }
 }
