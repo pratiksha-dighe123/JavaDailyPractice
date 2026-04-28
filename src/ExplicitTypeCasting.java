4bc46ddda8c647e3cc4027f4c3b416aa747b93da
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-04-28 23:13:30 +0530

    Explicit TypeCasting

---
+++ src/ExplicitTypeCasting.java
@@ -0,0 +1,9 @@
+public class ExplicitTypeCasting {
+    public static void main(String[] args) {
+        double x = 10.75;
+        int y = (int) x;  // manual conversion
+
+        System.out.println("Double value: " + x);
+        System.out.println("Converted to int: " + y);
+    }
+}
