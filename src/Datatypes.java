8e5d8d432b1458b231c40cd6fc328f141ed0b482
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-05-01 22:43:34 +0530

    Datatypes

---
+++ src/Datatypes.java
@@ -0,0 +1,28 @@
+public class Datatypes {
+    public static void main(String[] args) {
+
+        // Integer type
+        int number = 10;
+
+        // Floating point types
+        float decimal1 = 5.5f;
+        double decimal2 = 10.123;
+
+        // Character type
+        char letter = 'A';
+
+        // Boolean type
+        boolean isJavaFun = true;
+
+        // String (not primitive, but commonly used)
+        String message = "Hello, Java!";
+
+        // Printing values
+        System.out.println("Integer value: " + number);
+        System.out.println("Float value: " + decimal1);
+        System.out.println("Double value: " + decimal2);
+        System.out.println("Character value: " + letter);
+        System.out.println("Boolean value: " + isJavaFun);
+        System.out.println("String value: " + message);
+    }
+}
