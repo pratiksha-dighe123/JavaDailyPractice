cad6913e2e2fd330e684d27048ee819eda67a283
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-05-02 23:44:25 +0530

    Typecasting

--- src/TypeCasting.java
+++ src/TypeCasting.java
@@ -1,14 +1,14 @@
 public class TypeCasting {
     public static void main(String[] args) {
 
-        // Implicit Casting (Widening)
+        //Implicit Casting (Widening)
         int num = 100;
         double bigNum = num;   // int → double (automatic)
 
         System.out.println("Integer value: " + num);
         System.out.println("Converted to double: " + bigNum);
 
-        // Explicit Casting (Narrowing)
+        //Explicit Casting (Narrowing)
         double decimal = 9.78;
         int smallNum = (int) decimal;   // double → int (manual)
 
