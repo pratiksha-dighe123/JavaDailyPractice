d0bc8fd2d74b06707bbc531e838edf8faa3b839c
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-05-03 18:32:28 +0530

    if-else program

---
+++ src/IfElseExample.java
@@ -0,0 +1,12 @@
+public class IfElseExample {
+    public static void main(String[] args) {
+
+        int num = 7;
+
+        if (num % 2 == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+    }
+}
