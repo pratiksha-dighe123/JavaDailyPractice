70243a0d403128d34da2bc60d5a3d36b00dc01fe
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-05-05 18:38:12 +0530

    Object Oriented Programming concept

--- src/Oop.java
+++ src/Oop.java
@@ -1,21 +1,25 @@
-
-class Operations{
-
-    public int add(int n1, int n2){
-        int result= n1+n2;
-        return result;
+class Music{
+    public void playMusic(){
+        System.out.println("Music is playing");
     }
 }
 
-public class Oop {
+class Movie{
+    public String movieName(String name){
+        return name;
+    }
+}
 
+
+public class Oop{
     public static void main(String[] args){
 
-//        int num1= 10;
-//        int num2= 20;
+        Music m1= new Music();
+        m1.playMusic();
 
-        Operations opr=  new Operations();
-        int sum= opr.add(10,10);
-        System.out.println("The sum is:" +sum);
+        Movie m2= new Movie();
+        String name= m2.movieName("Dosti");
+        System.out.println("Movie name is: " +name);
+
     }
 }
