3a6f7c485bdeec92d92edba94a82ffb76a91f7fd
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-05-05 17:56:54 +0530

    Object Oriented Programming

---
+++ src/Oop.java
@@ -0,0 +1,15 @@
+
+public class Oop {
+
+    public static void main(string[] args){
+
+        int num1= 10;
+        int num2= 20;
+
+        int sum= num1+num2;
+        System.out.println("The sum is: " +sum);
+
+
+    }
+
+}
