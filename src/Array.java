a120c37fb073ac77310d497b567b5d846ad49cb5
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-05-07 13:36:47 +0530

    Reverse Array

--- src/Array.java
+++ src/Array.java
@@ -2,16 +2,14 @@ import java.util.Scanner;
 
 public class Array {
 
-    public static void main(String[] args){
-       int num[]= {10,20,30,30,40};
+    public static void main(String[] args) {
+        int num[] = {10, 20, 30, 40, 50};
 
-       int sum=0;
+        System.out.println("Reverse an array:");
 
-       for(int i=0; i<num.length; i++){
-           sum= sum+num[i];
-       }
-
-        System.out.println("Sum of the array is: " +sum);
+        for (int i = num.length-1 ; i >= 0; i--) {
+            System.out.println(num[i]);
+        }
     }
 
 }
