60a44fc848e787bba32f83f5c9307c09624b44ff
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-05-07 19:14:58 +0530

    String

---
+++ src/String_Examples.java
@@ -0,0 +1,17 @@
+public class String_Examples {
+
+    public static void main(String[] args){
+        String name= "Pratiksha";
+        System.out.println(name);
+        System.out.println("Length: " +name.length());
+        System.out.println("Uppercase: " +name.toUpperCase());
+        System.out.println("Lowercase: " +name.toLowerCase());
+        System.out.println(("Character at index: " +name.charAt(2)));
+        System.out.println("Substring: " +name.substring(0,3));
+        System.out.println("Replace: " +name.replace("sha", " "));
+        System.out.println("Contains: "+name.contains("tik"));
+        System.out.println("Starts with S?: " +name.startsWith("S"));
+        System.out.println("Ends with a?: " +name.endsWith("a"));
+    }
+
+}
