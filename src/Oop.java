24bc7dd7c1a82a4f9383ca91930669d09400c2cf
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-05-07 08:11:39 +0530

    Object Oriented Programming concept

--- src/Oop.java
+++ src/Oop.java
@@ -1,25 +1,25 @@
-class Music{
-    public void playMusic(){
-        System.out.println("Music is playing");
+
+class Computer{
+    public String playMusic(){
+        return "Music is playing...";
     }
 }
 
-class Pen{
-    public String buyPen(int cost){
-        if(cost >= 10)
-            return "Pen is bought ";
-        else
-            return "Pen is not available";
+class Store{
+    public int getItem(int cost){
+        return cost;
     }
 }
+
 
 public class Oop{
     public static void main(String[] args){
 
-        Music m1= new Music();
-        m1.playMusic();
+        Computer c1= new Computer();
+        c1.playMusic();
 
-        Pen p1= new Pen();
-        System.out.println(p1.buyPen(100));
+        Store s1= new Store();
+        s1.getItem(100);
+
     }
 }
