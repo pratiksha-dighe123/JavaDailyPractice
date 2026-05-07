b1ce10c2e5c6ac450680983e1a13d6118e6a99a6
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-05-07 13:14:34 +0530

    Store and Print Array Elements

--- src/Array.java
+++ src/Array.java
@@ -1,15 +1,22 @@
-import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Array {
 
     public static void main(String[] args){
-       int nums[][]= new int[3][4];
+       Scanner sc= new Scanner(System.in);
 
-       for(int i=0; i<3;i++){
-           for (int j=0; j<4; j++){
-               System.out.print(nums[i][j] + " ");
-           }
-           System.out.println();
+       int arr[]= new int[5];
+
+       System.out.println("Enter 5 numbers...: ");
+
+       for(int i=0; i<arr.length; i++){
+           arr[i]= sc.nextInt();
+       }
+
+       System.out.println("Array elements are...: ");
+
+       for(int i=0; i< arr.length; i++){
+           System.out.println(arr[i]);
        }
     }
 
