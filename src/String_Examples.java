156a83c78c6dcb6b96ea964d3f2d7d53684a9131
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-05-08 22:41:29 +0530

    String examples with methods

--- src/String_Examples.java
+++ src/String_Examples.java
@@ -4,8 +4,8 @@ public class String_Examples {
         String name= "Pratiksha";
         System.out.println(name);
         System.out.println("Length: " +name.length());
-        System.out.println("Uppercase: " +name.toUpperCase());
-        System.out.println("Lowercase: " +name.toLowerCase());
+        System.out.println("Uppercase : " +name.toUpperCase());
+        System.out.println("Lowercase : " +name.toLowerCase());
         System.out.println(("Character at index 0: " +name.charAt(2)));
         System.out.println("Substring: " +name.substring(0,3));
         System.out.println("Replace: " +name.replace("sha", " "));
