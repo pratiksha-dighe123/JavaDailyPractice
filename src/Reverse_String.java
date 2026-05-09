5b199a5f10be5a6d0d6d2c89570233f8b609f30e
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-05-09 21:53:55 +0530

    String Reverse

---
+++ src/Reverse_String.java
@@ -0,0 +1,11 @@
+public class Reverse_String {
+    public static void main(String[] args){
+        String name= "Pratiksha";
+        String reversed= "";
+        for(int i=name.length()-1; i>=0; i--){
+            reversed += name.charAt(i);
+        }
+        System.out.println("Original String: " +name);
+        System.out.println("Reversed String: " +reversed);
+    }
+}
