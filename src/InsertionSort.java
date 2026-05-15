b59584266c21436fcf1149f32ef0034f4632b2e6
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-05-15 22:30:44 +0530

    Insertion Sort

---
+++ src/InsertionSort.java
@@ -0,0 +1,33 @@
+public class InsertionSort {
+
+    public static void main(String[] args) {
+
+        int arr[] = {64, 25, 12, 2, 24};
+
+        System.out.println("Before Sorting:");
+
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        // Insertion Sort Logic
+        for(int i = 1; i < arr.length; i++) {
+
+            int key = arr[i];
+            int j = i - 1;
+
+            while(j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+
+        System.out.println("\nAfter Sorting:");
+
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+}
