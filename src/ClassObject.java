1730ed8b7cfa03a6aa8e589c7da73e74488cd401
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-05-17 23:07:12 +0530

    Class and Object

---
+++ src/ClassObject.java
@@ -0,0 +1,21 @@
+class ClassObject {
+
+    // Data members
+    int id = 101;
+    String name = "Pratiksha";
+
+    // Method
+    void display() {
+        System.out.println("Student ID: " + id);
+        System.out.println("Student Name: " + name);
+    }
+
+    public static void main(String[] args) {
+
+        // Creating object
+        Student s1 = new Student();
+
+        // Calling method
+        s1.display();
+    }
+}
