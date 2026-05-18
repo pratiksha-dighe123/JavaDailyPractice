eb105c83b2cde55cda1b8af4c90bae9aff1c1fba
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-05-18 19:10:12 +0530

    Inheritance Practice

---
+++ src/InheritanceProgram.java
@@ -0,0 +1,27 @@
+// Parent class
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+// Child class
+class Dog extends Animal {
+
+    void bark() {
+        System.out.println("Dog barks");
+    }
+
+    public static void main(String[] args) {
+
+        // Creating object of child class
+        Dog d = new Dog();
+
+        // Calling parent class method
+        d.sound();
+
+        // Calling child class method
+        d.bark();
+    }
+}
