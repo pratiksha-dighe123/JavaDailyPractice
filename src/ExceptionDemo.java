ba4280ba0ee67e143cefccda056d216be3726b84
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-06-25 23:13:02 +0530

    Encapsulation practice

---
+++ src/ExceptionDemo.java
@@ -0,0 +1,13 @@
+public class ExceptionDemo {
+    public static void main(String[] args) {
+
+        try {
+            int a = 10 / 0;
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+
+        System.out.println("Program Continues..");
+    }
+}
