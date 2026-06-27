637499be830261a150cfdb53062d447862fa3188
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-06-27 16:23:58 +0530

    List Interface

--- src/com/collection/ListInterface.java
+++ src/com/collection/ListInterface.java
@@ -5,13 +5,21 @@ import java.util.List;
 
 public class ListInterface {
     public static void main(String[] args){
-        List<String> list= new ArrayList<>();
+//        List<String> list= new ArrayList<>();
+//        list.add("Ram");
+//        list.add("Shyam");
+//        list.add("Priya");
+//        System.out.println("List elements:" + list);
 
-        list.add("Ram");
-        list.add("Shyam");
-        list.add("Priya");
+        List<Integer> list= new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        for(int i=0; i<list.size(); i++){
+            System.out.println(list.get(i));
+        }
 
-        System.out.println("List elements:" + list);
 
     }
 }
