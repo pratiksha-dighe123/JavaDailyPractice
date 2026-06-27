daafd589e37df3cde66f48c2d2285569653027dd
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-06-27 19:10:21 +0530

    Stack operations

---
+++ src/com/collection/StackDemo.java
@@ -0,0 +1,23 @@
+package com.collection;
+
+import java.util.Stack;
+
+public class StackDemo {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println(stack);
+
+        System.out.println(stack.pop());
+
+        System.out.println(stack.peek());
+
+        System.out.println(stack);
+    }
+}
