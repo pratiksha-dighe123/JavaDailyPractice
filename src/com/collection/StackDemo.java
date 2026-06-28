8796b4e3b2c8b918211ba5a3fb43a97b1b96f39a
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-06-28 23:02:11 +0530

    Stack operations

--- src/com/collection/StackDemo.java
+++ src/com/collection/StackDemo.java
@@ -11,7 +11,7 @@ public class StackDemo {
         stack.push(10);
         stack.push(20);
         stack.push(30);
-        stack.push(40 );
+        stack.push(40);
 
         System.out.println(stack);
 
