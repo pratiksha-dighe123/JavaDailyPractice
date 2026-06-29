98d084c74cd68e869d3ce4aef27c4ba21fcbbca1
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-06-29 14:11:42 +0530

    Queue Program

---
+++ src/com/collection/QueueDemo.java
@@ -0,0 +1,22 @@
+package com.collection;
+import java.util.*;
+
+public class QueueDemo {
+
+        public static void main(String[] args) {
+
+            Queue<Integer> q = new LinkedList<>();
+
+            q.offer(10);
+            q.offer(20);
+            q.offer(30);
+
+            System.out.println(q);
+
+            System.out.println(q.poll());
+
+            System.out.println(q);
+
+            System.out.println(q.peek());
+        }
+}
