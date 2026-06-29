115b3f0d0e9b9d501a7fedf3906c3879274aa27c
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-06-29 13:43:58 +0530

    HashSet Program

---
+++ src/com/collection/TreeSetDemo.java
@@ -0,0 +1,19 @@
+package com.collection;
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+        public static void main(String[] args) {
+
+            TreeSet<Integer> set = new TreeSet<>();
+
+            set.add(50);
+            set.add(20);
+            set.add(40);
+            set.add(10);
+            set.add(30);
+
+            System.out.println(set);
+        }
+
+}
