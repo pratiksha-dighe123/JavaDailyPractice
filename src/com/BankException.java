cd3e6df6c51af575b334b8802b2f7afb0e8f81bb
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-07-01 15:07:28 +0530

    Exception Handling

---
+++ src/com/BankException.java
@@ -0,0 +1,29 @@
+package com;
+
+class InsufficientBalanceException extends Exception{
+    public InsufficientBalanceException(String message){
+        super(message);
+    }
+}
+
+public class BankException {
+    static void withdraw(int balance, int amount) throws InsufficientBalanceException{
+        if(amount> balance) {
+            throw new InsufficientBalanceException("Insufficient Balance...");
+        }
+        System.out.println("Transaction Successful");
+        System.out.println("Remaining Balance = " + (balance - amount));
+        }
+
+    public static void main(String[] args) {
+        int balance= 1000;
+        int amount= 1500;
+        try{
+            withdraw(balance, amount);
+        } catch (InsufficientBalanceException e) {
+
+            System.out.println(e.getMessage());
+
+        }
+    }
+}
