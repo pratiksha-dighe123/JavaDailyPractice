12162272cc13f39549de0332429373933dfe01a4
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-07-01 15:12:59 +0530

    Thread Demo

---
+++ src/com/multithreading/ThreadDemo.java
@@ -0,0 +1,17 @@
+package com.multithreading;
+
+class Mythread extends Thread{
+    public void run(){
+        System.out.println("Thread is running...");
+    }
+}
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+
+        Mythread t = new Mythread();
+        t.start();
+
+        System.out.println("Main thread is running...");
+    }
+}
