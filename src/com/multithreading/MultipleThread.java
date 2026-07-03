414d6ba3743a352f0059a79087c51c1864e1d1cc
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-07-03 23:13:45 +0530

    multiple threads

---
+++ src/com/multithreading/MultipleThread.java
@@ -0,0 +1,28 @@
+package com.multithreading;
+
+class Demo extends Thread {
+
+    public void run() {
+
+        for(int i=1;i<=5;i++) {
+
+            System.out.println(getName()+" : "+i);
+
+        }
+    }
+}
+
+public class MultipleThread {
+
+    public static void main(String[] args) {
+
+        Demo t1=new Demo();
+
+        Demo t2=new Demo();
+
+        t1.start();
+
+        t2.start();
+
+    }
+}
