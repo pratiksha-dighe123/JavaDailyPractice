644b622b362b06a9f57137d4e09234ea210db862
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-07-17 23:39:42 +0530

    Synchronization

--- src/com/multithreading/SynchronizeDemo.java
+++ src/com/multithreading/SynchronizeDemo.java
@@ -1,4 +1,69 @@
 package com.multithreading;
 
+class Table {
+
+    synchronized void printTable(int n){
+
+        for(int i=1;i<=5;i++){
+
+            System.out.println(n*i);
+
+        }
+
+    }
+
+}
+
+class MyThread1 extends Thread{
+
+    Table t;
+
+    MyThread1(Table t){
+
+        this.t=t;
+
+    }
+
+    public void run(){
+
+        t.printTable(5);
+
+    }
+
+}
+
+class MyThread2 extends Thread{
+
+    Table t;
+
+    MyThread2(Table t){
+
+        this.t=t;
+
+    }
+
+    public void run(){
+
+        t.printTable(10);
+
+    }
+
+}
+
 public class SynchronizeDemo {
+
+    public static void main(String[] args){
+
+        Table obj=new Table();
+
+        MyThread1 t1=new MyThread1(obj);
+
+        MyThread2 t2=new MyThread2(obj);
+
+        t1.start();
+
+        t2.start();
+
+    }
+
 }
