3ce07d4beffa89f1b2ee665824edf09d3fb347f6
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-07-20 21:47:04 +0530

    Synchronization

--- src/com/multithreading/SynchronizeDemo.java
+++ src/com/multithreading/SynchronizeDemo.java
@@ -56,11 +56,11 @@ public class SynchronizeDemo {
 
         Table obj=new Table();
 
-        MyThread1 t=new MyThread1(obj);
+        MyThread1 t1=new MyThread1(obj);
 
         MyThread2 t2=new MyThread2(obj);
 
-        t.start();
+        t1.start();
 
         t2.start();
 
